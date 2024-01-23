cc19429c6b5b3df1168f26a41b164a3cea02397c
shiv9568 <shivcollage9568.com>
2024-01-23 19:56:11 +0530

    Change portfolio name To Shivansh

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export const Navbar = () => {
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href="/">
-        Portfolio
+        Shivansh
       </a>
       <div className={styles.menu}>
         <img
